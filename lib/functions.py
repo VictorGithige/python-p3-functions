3ec286033e3be1e525f27910929e4b182cacfa1c
VictorGithige <muhuthiagithige1@gmail.com>
2024-03-08 17:25:19 +0300

    final commit

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,29 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
+    print("Hello, programmer!")
     pass
 
 def greet(name):
+    print(f"Hello, {name}!")
     pass
+greet("James")
+
 
 def greet_with_default(name="programmer"):
+    print(f"Hello, {name}!")
     pass
+greet_with_default('jerry')
 
 def add(num1, num2):
+    return num1 + num2
     pass
+sum = add(23, 25)
+print(sum)
+
 
 def halve(number):
+    return number / 2
     pass
+half = halve(20)
+print(half)
